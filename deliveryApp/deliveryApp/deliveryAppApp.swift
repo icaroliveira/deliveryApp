b5d483dae9f6d6b4dcdd4e0804de032a7688ec72
Ícaro Rangel <107810342+icaroliveira@users.noreply.github.com>
2023-06-28 03:49:26 -0400

    commit inicial

---
+++ deliveryApp/deliveryApp/deliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  deliveryAppApp.swift
+//  deliveryApp
+//
+//  Created by icaro on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct deliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
