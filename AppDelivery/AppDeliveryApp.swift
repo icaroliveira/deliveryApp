068cdaf65abb66239aae5049c287fb58b1adc7f9
Ícaro Rangel <107810342+icaroliveira@users.noreply.github.com>
2023-06-28 03:40:34 -0400

    commit
    commit inicial

---
+++ AppDelivery/AppDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDeliveryApp.swift
+//  AppDelivery
+//
+//  Created by icaro on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDelivery/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  AppDelivery
+//
+//  Created by icaro on 28/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Text("teste")
+        
+        
+        
+        
+        
+        
+        
+ 
+        
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
