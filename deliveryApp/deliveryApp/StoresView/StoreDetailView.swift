378a72b694a8e1b3abf0c25d6cd02c29bac6a70c
Ícaro Rangel <107810342+icaroliveira@users.noreply.github.com>
2023-07-10 09:35:45 -0400

    assets

---
+++ deliveryApp/deliveryApp/StoresView/StoreDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  StoreDetailView.swift
+//  deliveryApp
+//
+//  Created by icaro on 09/07/23.
+//
+
+import SwiftUI
+
+struct StoreDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StoreDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreDetailView()
+    }
+}
