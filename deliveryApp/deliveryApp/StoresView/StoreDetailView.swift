b630dc17aef8dd1f5dfe2cac6ff6c9cbed1cb89d
icaro rangel <icarogin2@gmail.com>
2023-08-24 17:58:52 -0400

    Criaçao da tela de detalhes do restaurante

--- deliveryApp/deliveryApp/StoresView/StoreDetailView.swift
+++ deliveryApp/deliveryApp/StoresView/StoreDetailView.swift
@@ -11,7 +11,29 @@ struct StoreDetailView: View {
     
     let store: StoreType
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading){
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            HStack{
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                
+                Spacer()
+                
+                Image(store.logoImage)
+            }
+            .padding(.vertical,8)
+            .padding(.horizontal)
+            
+            HStack{
+                Text(store.location)
+                ForEach(1...store.stars, id:\.self) { _ in
+                    Image(systemName:"star.fill" )
+                }
+            }
+        }
     }
 }
 
