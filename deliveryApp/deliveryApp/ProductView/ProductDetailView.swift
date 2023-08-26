b90a61d25132e25ae861338e218091d6f6665b9d
icaro rangel <icarogin2@gmail.com>
2023-08-26 09:01:08 -0400

    Detalhes do produto
    implementando mais coisas para a tela de detalhes de produto

--- deliveryApp/deliveryApp/ProductView/ProductDetailView.swift
+++ deliveryApp/deliveryApp/ProductView/ProductDetailView.swift
@@ -12,6 +12,44 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
+        
+        VStack{
+            VStack(alignment: .leading){
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 20)
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                Text(product.description)
+                    .padding(.horizontal)
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+            Spacer()
+            Button{
+                //pode colocar algum print
+            }label: {
+                HStack{
+                    Image(systemName: "cart")
+                    
+                    Text("Adicionar ao carrinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical,16)
+                .font(.title3)
+                .bold()
+                .background(Color("ColorRed"))
+                .foregroundColor(.white)
+                .cornerRadius(32)
+                .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6,y: 8)
+            }
+        }
+        
         Text(product.name)
     }
 }
