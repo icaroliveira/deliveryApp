9f9788f735402668720037615e58500604216fed
icaro rangel <icarogin2@gmail.com>
2023-08-31 03:47:19 -0400

    componentizando

---
+++ deliveryApp/deliveryApp/StoresView/StoredetailView/StoreDetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  StoreDetailHeaderView.swift
+//  deliveryApp
+//
+//  Created by icaro on 31/08/23.
+//
+
+import SwiftUI
+
+struct StoreDetailHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StoreDetailHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreDetailHeaderView()
+    }
+}
